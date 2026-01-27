282bd229a8891149fc9db5ddf64fb7ff916cdbde
moghanifatemeh7-lang <moghanifatemeh7@gmail.com>
2026-01-27 19:38:32 +0330

    Update p11.py

--- p11.py
+++ p11.py
@@ -1,4 +1,14 @@
-num=int(input("adad vared kon:"))
-for i in range(1,num):
-    if i %2==0:
-        print(i)
+a=int(input("adad vared kon:"))
+b=int(input("adad vared kon:"))
+start,end= min(a,b),max(a,b)
+c=0
+for i in range(start,end+1):
+              c=0
+              for j in range (2,i//2):
+                       if i % j==0:
+                         c+=1
+                         break
+              if c==0:
+                        print(i,"is prime")
+              else:
+                        print(i,"is not prime")
